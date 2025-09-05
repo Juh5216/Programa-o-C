97d06472ebfc929e02ecd74c2ee152bdc6bcac25
Juh5216 <161980749+Juh5216@users.noreply.github.com>
2025-09-05 15:39:25 +0000

    - Inclusão de estruturas decisão

--- GenciadorDeTarefas.c
+++ GenciadorDeTarefas.c
@@ -8,7 +8,7 @@
 int main() {
 
     int opcao;
-    float nota1, nota2, nota3;
+    float nota1, nota2, nota3, media;
 
     printf("Menu de gerenciamento de estudadantes \n");
     printf("Escolha uma opção: \n");
@@ -27,16 +27,30 @@ int main() {
 
         // Teste de condição: nota é >= 0 e <=10
             if (nota1 >= 0 && nota2 <= 10){
-                printf("Entrada correta!");
+                media = (nota1+nota2/2);
+                printf("A média é %.2f \n", media);
 
             } else {
-                printf("Entrada com valores errados");
+                printf("Entrada com valores errados de notas");
             
             }
 
         break;
         case 2:
         printf("Determinar status \n");
+        printf("Entre com a média:");
+        scanf("%f", &media);
+        // Com operador ternário: media >=5 ? printf("Aprovado! \n") : printf("Reprovado!");
+        if (media>=7) {
+            printf("Aprovado!");
+        } else if (media >=5 && media <=6) {
+            printf("Recuperação!");
+
+        } else {
+            printf("Reprovado!");
+
+         }
+
         break;
         case 3:
         printf("Saindo do programa \n");
