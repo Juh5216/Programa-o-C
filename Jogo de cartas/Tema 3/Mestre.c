9151dcfa03aea4bd43e051271fce32f59a5c2751
Juh5216 <161980749+Juh5216@users.noreply.github.com>
2025-09-09 16:22:38 +0000

    - Inclusão dos passos no nível 'Mestre'

---
+++ Jogo de cartas/Tema 3/Mestre.c
@@ -0,0 +1,18 @@
+/*  - O jogador deve escolher 2 atributos numéricos DIFERENTES para a comparação de cartas, através de MENUS INTERATIVOS (Switch)
+        * O jogador não pode selecionar o mesmo atributo 2 vezes
+    - Comparar os atributos (Todos) - Comparação com Múltiplos Atributos: Implemente a lógica para comparar as duas cartas com base nos dois atributos escolhidos.
+    - Soma dos Atributos: Após comparar os dois atributos individualmente, o sistema deve somar os valores dos atributos para cada carta. A carta com a maior soma vence a rodada.
+    - Tratamento de Empates: Implemente a lógica para tratar empates. Se a soma dos atributos das duas cartas for igual, exiba a mensagem "Empate!".
+    - Menus Dinâmicos: Crie menus interativos e dinâmicos usando switch. "Dinâmicos" significa que, por exemplo, após o jogador escolher o primeiro atributo, esse atributo não deve mais aparecer como opção para a escolha do segundo atributo.
+    - Exibição Clara do Resultado: Mostre o resultado da comparação de forma clara e organizada, incluindo:
+ 
+O nome dos dois países.
+ 
+Os dois atributos usados na comparação.
+ 
+Os valores de cada atributo para cada carta.
+ 
+A soma dos atributos para cada carta.
+ 
+Qual carta venceu (ou se houve empate).
+ */
