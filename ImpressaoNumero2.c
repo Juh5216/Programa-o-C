5543b6148c7d4206942d44419bd7051597a3af0f
Juh5216 <161980749+Juh5216@users.noreply.github.com>
2025-09-12 20:25:58 +0000

    - Ajustes no código

--- ImpressaoNumero2.c
+++ ImpressaoNumero2.c
@@ -10,10 +10,10 @@ int main() {
             scanf(" %d", &numero);
 
             if (numero % 2 ==0) {
-                printf(" %d é par", numero);
+                printf(" %d é par. \n", numero);
 
             } else {
-                printf(" %d é ímpar", numero);
+                printf(" %d é ímpar. \n", numero);
 
             }
 
