70d7a6dcb5e39d30888bc70676707b071c807894
Juh5216 <161980749+Juh5216@users.noreply.github.com>
2025-09-15 01:44:25 +0000

    - Ajuste dos erros de sintaxe e adição de mensagens de confirmação de movimento.

--- Xadrez/novato.c
+++ Xadrez/novato.c
@@ -9,52 +9,6 @@
 // Bispo - 5 casas na diagonal (Direita)
 // Rainha - 8 casas para esquerda
 
-
-#include<stdio.h>
-
-int main() {
-
-
-        
-    printf("Escolha a peça que deseja movimentar: \n");
-    printf("1- Torre \n");
-    printf("2- Bispo \n");
-    printf("3- Rainha \n");
-    int peca;
-    scanf("%d", &peca);
-
-        switch (peca)
-        {
-        case 1:
-
-        // Torre
-            for (int T = 0; T<=5; T++) {
-                printf("Cima\n");
-        break;
-
-        case 2:
-
-        //Bispo
-            for (int B = 0; B<=5; B++) {
-                printf("Cima Direita\n");
-        break;
-        case 3:
-
-        // Rainha
-            for (int R = 0; R<=8; R++) {
-                 printf("Esquerda\n");
-        
-        default:
-            printf("Peça inválida\n");
-            break;
-        }
-
-
-   
-}
-
-
-
 /* Saída de Dados: 
     O programa deverá imprimir no console a direção do movimento a cada casa percorrida pela peça. 
         - Para movimentos na diagonal (Bispo), imprimir a combinação de duas direções. 
@@ -67,5 +21,46 @@ int main() {
             * printf("Cima Esquerda\n"); 
             * printf("Direita\n"); */
 
- return 0;
+#include<stdio.h>
+
+int main() {
+
+    printf("Escolha a peça que deseja movimentar: \n");
+    printf("1- Torre \n");
+    printf("2- Bispo \n");
+    printf("3- Rainha \n");
+    int peca;
+    scanf("%d", &peca);
+
+        switch (peca){
+        case 1:
+
+        // Torre
+            for (int T = 0; T<=5; T++) 
+                printf("Cima\n");
+                printf("Movimento concluído!\n");
+        break;
+        case 2:
+
+        //Bispo
+            for (int B = 0; B<=5; B++) 
+                printf("Cima e direita\n");
+                printf("Movimento concluído!\n");
+        break;
+        case 3:
+
+        // Rainha
+            for (int R = 0; R<=8; R++) 
+                 printf("Esquerda\n");
+                 printf("Movimento concluído!\n");
+        
+        break;
+        default:
+            printf("Peça inválida, escolha novamente.\n");
+            break;
+        
+}
+        
+return (0);
+
 }
