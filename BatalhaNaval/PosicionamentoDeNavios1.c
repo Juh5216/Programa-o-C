5b152b72d3cab8156a3c5bb847203425e1928a2f
Juh5216 <161980749+Juh5216@users.noreply.github.com>
2025-09-25 21:10:50 +0000

    - Correção de comentarios

--- BatalhaNaval/PosicionamentoDeNavios1.c
+++ BatalhaNaval/PosicionamentoDeNavios1.c
@@ -1,25 +1,11 @@
 #include <stdio.h>
 
-/*  Representando o tabuleiro
-        - O tabuleiro terá um tamanho fixo 10x10
-        - Inicialize todas as posições do tabuleiro com o valor 0, representando água
-
-    Posicionamento dos navios
-        - Declare e inicialize dois vetores (arrays unidimensionais) para representar os navios
-        - Cada navio ocupará um número fixo de posições no tabuleiro (defina esse tamanho, por exemplo, 3 posições)
-        - Um navio será posicionado horizontalmente e o outro verticalmente
-        - Represente as posições ocupadas pelos navios na matriz do tabuleiro com o valor 3
-        - Você deverá escolher as coordenadas iniciais de cada navio e garantir que eles estejam completamente dentro dos limites do tabuleiro e não se sobreponham
-        - Dica: O posicionamento do navio pode ser feito copiando o valor 3 de cada posição do vetor do navio para as posições correspondentes na matriz do tabuleiro, de acordo com a orientação (horizontal ou vertical) do navio.
-
-    Exibição do tabuleiro
-        - Utilize loops aninhados e o comando printf para exibir o tabuleiro no console. 
-        - Mostre a matriz completa, com 0s representando água e 3s representando as partes dos navios
-*/
-
 int main() {
 
-    //Tabuleiro
+    /* Representando o tabuleiro
+        - O tabuleiro terá um tamanho fixo 10x10
+        - Inicialize todas as posições do tabuleiro com o valor 0, representando água */
+
     int tabuleiro [10][10];
     int i, j;
 
@@ -34,7 +20,13 @@ int main() {
     }
     
 
-    //Posicionamento dos navios
+    /*Posicionamento dos navios
+        - Declare e inicialize dois vetores (arrays unidimensionais) para representar os navios
+        - Cada navio ocupará um número fixo de posições no tabuleiro (defina esse tamanho, por exemplo, 3 posições)
+        - Um navio será posicionado horizontalmente e o outro verticalmente
+        - Represente as posições ocupadas pelos navios na matriz do tabuleiro com o valor 3
+        - Você deverá escolher as coordenadas iniciais de cada navio e garantir que eles estejam completamente dentro dos limites do tabuleiro e não se sobreponham
+        - Dica: O posicionamento do navio pode ser feito copiando o valor 3 de cada posição do vetor do navio para as posições correspondentes na matriz do tabuleiro, de acordo com a orientação (horizontal ou vertical) do navio. */
 
     int navio1 [3] = {3, 3, 3}; //Navio horizontal
     int navio2 [3] = {3, 3, 3}; //Navio vertical
@@ -53,8 +45,10 @@ int main() {
         tabuleiro [linha_n2 + i][coluna_n2] = navio2 [i];
     }
     
-    //Exibição do tabuleiro
-
+    /*  Exibição do tabuleiro
+        - Utilize loops aninhados e o comando printf para exibir o tabuleiro no console. 
+        - Mostre a matriz completa, com 0s representando água e 3s representando as partes dos navios */
+        
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             printf("%d ", tabuleiro [i][j]);
